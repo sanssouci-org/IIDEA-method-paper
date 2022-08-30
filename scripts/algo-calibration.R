4afcd1527bac473de19cb71decfe12e9b1ffcd85
Pierre Neuvial <pierre.neuvial@math.cnrs.fr>
2022-08-31 00:04:34 +0200

    cleanups in figure names

--- scripts/algo-calibration.R
+++ scripts/algo-calibration.R
@@ -1,3 +1,3 @@
 library("tinytex")
 pdflatex("scripts/algo-calibration/algo-calibration.tex", 
-         pdf_file = "figures/fig-1-algo-calibration.pdf")
+         pdf_file = "figures/fig-1_algo-calibration.pdf")
